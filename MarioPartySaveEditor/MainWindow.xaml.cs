39eef474c84a750f17350a3b699c0d466c780b7e
Riley Strickland <Github@rileystrickland.com>
2018-10-05 03:35:48 -0400

    Upload 1
    I spent more time messing around with wpf than on the actual code.

---
+++ MarioPartySaveEditor/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+
+namespace MarioPartySaveEditor
+{
+
+    public partial class MainWindow : MetroWindow
+    {
+        GameSave Save;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenSaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                if (GameSave.Read(openFileDialog.FileName, out Save))
+                {
+                    MessageBox.Show("Save is valid ... Loaded.");
+                    try
+                    {
+                        DataContext = Save;
+                    }
+                    catch (Exception) { } //who cares
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Save == null)
+            {
+                MessageBox.Show("No Loaded Save !");
+                return;
+            }
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                if (GameSave.Save(Save, saveFileDialog.FileName))
+                {
+                    MessageBox.Show("... Saved !");
+
+                }
+            }
+        }
+    }
+}
---
+++ MarioPartySaveEditor/Save.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace MarioPartySaveEditor
+{
+    public class GameSave
+    {
+        public int Bucks { get; set; }
+        private byte[] contents;
+        private GameSave(byte[] Data)
+        {
+            contents = Data;
+            Bucks = BitConverter.ToInt32(contents, 0x0C);
+        }
+
+        public static bool Read(string Path, out GameSave Save)
+        {
+            Save = null;
+            if (!System.IO.File.Exists(Path)) return false;
+            byte[] Data = System.IO.File.ReadAllBytes(Path);
+
+            if (Data.Length != 0x2335C0) return false;
+
+            Save = new GameSave(Data);
+            return true;
+
+        }
+
+        public static bool Save(GameSave Save, string Path)
+        {
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream(Save.contents))
+            {
+                ms.Seek(0x0C, System.IO.SeekOrigin.Begin);
+                ms.Write(BitConverter.GetBytes(Save.Bucks), 0, 4);
+            }
+            System.IO.File.WriteAllBytes(Path, Save.contents);
+            return true;
+        }
+    }
+}
